6512fbebac0cf7f152b519d30b0c0b512e15c4dd
Aadi26701 <>
2023-05-04 07:11:38 +0000

    Updates

--- _next/static/zIuIZiNo2XdIbsPuJx00I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/878-aa040cdac4607e80.js","static/css/442ab822d3fe3ff1.css","static/chunks/pages/index-6e522aad4423abb0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
